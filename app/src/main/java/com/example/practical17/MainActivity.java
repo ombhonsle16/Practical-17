fc023328e73e61087239f182647acb30e9dfb67a
ombhonsle16 <ombhonsle16@gmail.com>
2022-06-02 08:44:03 +0530

    Practical 17

---
+++ app/src/main/java/com/example/practical17/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.practical17;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    EditText etUsername, etPassword;
+    Button btnStatus;
+    TextView tvLoginStatus;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        etUsername = (EditText) findViewById(R.id.etUsername);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        btnStatus = (Button) findViewById(R.id.button);
+        tvLoginStatus = (TextView) findViewById(R.id.tvLoginStatus);
+
+        btnStatus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                check();
+            }
+        });
+    }
+
+    public void check(){
+        if(etUsername.getText().toString().equals("tonystark") && etPassword.getText().toString().equals("loveyou3000")){
+            tvLoginStatus.setText("Login successful");
+        }else{
+            Toast.makeText(this, "Login fail", Toast.LENGTH_LONG).show();
+        }
+    }
+}
